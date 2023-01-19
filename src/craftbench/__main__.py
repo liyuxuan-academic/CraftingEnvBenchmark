6b49815e7a69bedfba234390422f5dbec4de2d14
YuxuanOnBeluga <li32@ualberta.ca>
2023-01-19 02:42:11 -0500

    [Fix] temporarily remove buggy print in __main__.py

--- src/craftbench/__main__.py
+++ src/craftbench/__main__.py
@@ -2,5 +2,6 @@ from craftbench.maskable_ppo import benchmark_mskppo
 
 
 def main():
-    print(f"{'CraftingEnvBenchmark': -^40}")
+    # print(f"{'CraftingEnvBenchmark': -^40}")
+    print("CraftingEnvBenchmark")
     benchmark_mskppo(save_req_graph=True, save_sol_graph=True)
